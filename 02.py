ecda8c6588f1b7b3156727a53ed8ccba77a737e3
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-04-19 22:04:27 +0530

    Add sales chart with styling - green dashed line with markers and legend

---
+++ 02.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x =["adii","khusahl","rahul","yashika"]
+y =[21,19,20,12]
+plt.plot(x,y)
+plt.title("age graph")
+plt.grid()
+plt.xlabel("name of the persion")
+plt.ylabel("age of the persion")
+plt.show()
